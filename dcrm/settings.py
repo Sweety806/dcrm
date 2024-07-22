60472a7d35b1024db5919c8665b360f1fb41270b
Sweety <sweetysh1211@gmail.com>
2024-07-22 18:36:41 +0530

    Final Project

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dcrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [],         #BASE_DIR ,"templates"
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# added manually
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+#       "/var/www/static/",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
